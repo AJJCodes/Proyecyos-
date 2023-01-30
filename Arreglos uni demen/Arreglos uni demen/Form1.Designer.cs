3cefc20befc5926c5f56e6c248a6c7057da90982
AnthonyJacko345 <63176790+FlappyBird227@users.noreply.github.com>
2023-01-29 20:51:49 -0600

    Add files via upload

---
+++ Arreglos uni demen/Arreglos uni demen/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Arreglos_uni_demen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTamaño = new System.Windows.Forms.TextBox();
+            this.btnguard = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btningresar = new System.Windows.Forms.Button();
+            this.Menors = new System.Windows.Forms.Button();
+            this.mayor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cantidad de numeros a ingresar";
+            // 
+            // txtTamaño
+            // 
+            this.txtTamaño.Location = new System.Drawing.Point(222, 23);
+            this.txtTamaño.Name = "txtTamaño";
+            this.txtTamaño.Size = new System.Drawing.Size(38, 23);
+            this.txtTamaño.TabIndex = 1;
+            // 
+            // btnguard
+            // 
+            this.btnguard.Location = new System.Drawing.Point(301, 22);
+            this.btnguard.Name = "btnguard";
+            this.btnguard.Size = new System.Drawing.Size(75, 23);
+            this.btnguard.TabIndex = 2;
+            this.btnguard.Text = "Guardar";
+            this.btnguard.UseVisualStyleBackColor = true;
+            this.btnguard.Click += new System.EventHandler(this.btnguard_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Numero";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(160, 67);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 23);
+            this.txtNumero.TabIndex = 4;
+            // 
+            // btningresar
+            // 
+            this.btningresar.Location = new System.Drawing.Point(301, 67);
+            this.btningresar.Name = "btningresar";
+            this.btningresar.Size = new System.Drawing.Size(75, 23);
+            this.btningresar.TabIndex = 5;
+            this.btningresar.Text = "Ingresar";
+            this.btningresar.UseVisualStyleBackColor = true;
+            this.btningresar.Click += new System.EventHandler(this.btningresar_Click);
+            // 
+            // Menors
+            // 
+            this.Menors.Location = new System.Drawing.Point(277, 123);
+            this.Menors.Name = "Menors";
+            this.Menors.Size = new System.Drawing.Size(131, 23);
+            this.Menors.TabIndex = 6;
+            this.Menors.Text = "Mostrar Menor";
+            this.Menors.UseVisualStyleBackColor = true;
+            this.Menors.Click += new System.EventHandler(this.Menors_Click);
+            // 
+            // mayor
+            // 
+            this.mayor.Location = new System.Drawing.Point(54, 123);
+            this.mayor.Name = "mayor";
+            this.mayor.Size = new System.Drawing.Size(133, 23);
+            this.mayor.TabIndex = 7;
+            this.mayor.Text = "Mostrar Mayor";
+            this.mayor.UseVisualStyleBackColor = true;
+            this.mayor.Click += new System.EventHandler(this.mayor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 185);
+            this.Controls.Add(this.mayor);
+            this.Controls.Add(this.Menors);
+            this.Controls.Add(this.btningresar);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnguard);
+            this.Controls.Add(this.txtTamaño);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTamaño;
+        private System.Windows.Forms.Button btnguard;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btningresar;
+        private System.Windows.Forms.Button Menors;
+        private System.Windows.Forms.Button mayor;
+    }
+}
+
---
+++ Arreglos uni demen/Arreglos uni demen/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arreglos_uni_demen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtNumero.Enabled = false;
+            btningresar.Enabled = false;
+            mayor.Enabled = false;
+            Menors.Enabled = false;
+        }
+
+        int []Numero;
+        int Contador, Mayor,Menor;
+
+        private void Menors_Click(object sender, EventArgs e)
+        {
+            Menor = Numero[0];
+            for (int i = 1; i < Contador; i++)
+            {
+                if (Numero[i] < Menor)
+                {
+                    Menor = Numero[i];
+                }
+            }
+            MessageBox.Show("El numero menor es : " + " " + Menor, "sistema");
+        }
+
+        private void btningresar_Click(object sender, EventArgs e)
+        {
+            if (Contador < Numero.Length)
+            {
+                Numero[Contador] = int.Parse(txtNumero.Text);
+                Contador++;
+                txtNumero.Text = " ";
+            }
+            else
+            {
+                txtNumero.Text = " ";
+                MessageBox.Show("Ya no hay espacio ", "sistema", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtNumero.Enabled = false;
+            }
+        }
+
+        private void mayor_Click(object sender, EventArgs e)
+        {
+            Mayor = Numero[0];
+            for(int i = 1; i < Contador; i++)
+            {
+                if (Numero[i]>Mayor)
+                {
+                    Mayor = Numero[i];
+                }
+            }
+            MessageBox.Show("El numero mayor es : " + " " + Mayor, "sistema");
+        }
+
+        private void btnguard_Click(object sender, EventArgs e)
+        {
+            int Tamaño;
+            try
+            {
+                Tamaño = int.Parse(txtTamaño.Text);
+                Numero = new int[Tamaño];
+                txtNumero.Enabled = true;
+                btningresar.Enabled = true;
+                mayor.Enabled = true;
+                Menors.Enabled = true;
+                txtTamaño.Enabled = false;
+                Contador = 0;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
