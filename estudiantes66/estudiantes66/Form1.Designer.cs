edbb98900983832dd9d1a37bdb08852b68c75a6d
AnthonyJacko345 <63176790+FlappyBird227@users.noreply.github.com>
2023-01-29 20:47:03 -0600

    Add files via upload

---
+++ estudiantes66/estudiantes66/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿
+namespace estudiantes66
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtapellido = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtmateD = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtmate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSocio = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtpoo = new System.Windows.Forms.TextBox();
+            this.btningresar = new System.Windows.Forms.Button();
+            this.btnmostrar = new System.Windows.Forms.Button();
+            this.Estudiantes = new System.Windows.Forms.ListBox();
+            this.btnmejoras = new System.Windows.Forms.Button();
+            this.btnasigna = new System.Windows.Forms.Button();
+            this.btnMJRE = new System.Windows.Forms.Button();
+            this.btnreproa = new System.Windows.Forms.Button();
+            this.Quit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(119, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(12, 33);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(101, 23);
+            this.txtnombre.TabIndex = 2;
+            // 
+            // txtapellido
+            // 
+            this.txtapellido.Location = new System.Drawing.Point(119, 33);
+            this.txtapellido.Name = "txtapellido";
+            this.txtapellido.Size = new System.Drawing.Size(100, 23);
+            this.txtapellido.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtmateD);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtmate);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtSocio);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtpoo);
+            this.groupBox1.Location = new System.Drawing.Point(288, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(500, 142);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Clases";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(315, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 21);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Matematicas Discretas";
+            // 
+            // txtmateD
+            // 
+            this.txtmateD.Location = new System.Drawing.Point(324, 43);
+            this.txtmateD.Name = "txtmateD";
+            this.txtmateD.Size = new System.Drawing.Size(156, 23);
+            this.txtmateD.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(218, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Matematica";
+            // 
+            // txtmate
+            // 
+            this.txtmate.Location = new System.Drawing.Point(218, 43);
+            this.txtmate.Name = "txtmate";
+            this.txtmate.Size = new System.Drawing.Size(100, 23);
+            this.txtmate.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(112, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 21);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Sociologia";
+            // 
+            // txtSocio
+            // 
+            this.txtSocio.Location = new System.Drawing.Point(112, 43);
+            this.txtSocio.Name = "txtSocio";
+            this.txtSocio.Size = new System.Drawing.Size(100, 23);
+            this.txtSocio.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(6, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 21);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "POO";
+            // 
+            // txtpoo
+            // 
+            this.txtpoo.Location = new System.Drawing.Point(6, 43);
+            this.txtpoo.Name = "txtpoo";
+            this.txtpoo.Size = new System.Drawing.Size(100, 23);
+            this.txtpoo.TabIndex = 9;
+            // 
+            // btningresar
+            // 
+            this.btningresar.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btningresar.Location = new System.Drawing.Point(12, 90);
+            this.btningresar.Name = "btningresar";
+            this.btningresar.Size = new System.Drawing.Size(97, 37);
+            this.btningresar.TabIndex = 5;
+            this.btningresar.Text = "Ingresar";
+            this.btningresar.UseVisualStyleBackColor = true;
+            this.btningresar.Click += new System.EventHandler(this.btningresar_Click);
+            // 
+            // btnmostrar
+            // 
+            this.btnmostrar.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnmostrar.Location = new System.Drawing.Point(135, 90);
+            this.btnmostrar.Name = "btnmostrar";
+            this.btnmostrar.Size = new System.Drawing.Size(97, 37);
+            this.btnmostrar.TabIndex = 6;
+            this.btnmostrar.Text = "Mostrar";
+            this.btnmostrar.UseVisualStyleBackColor = true;
+            this.btnmostrar.Click += new System.EventHandler(this.btnmostrar_Click);
+            // 
+            // Estudiantes
+            // 
+            this.Estudiantes.FormattingEnabled = true;
+            this.Estudiantes.ItemHeight = 15;
+            this.Estudiantes.Location = new System.Drawing.Point(12, 205);
+            this.Estudiantes.Name = "Estudiantes";
+            this.Estudiantes.Size = new System.Drawing.Size(755, 109);
+            this.Estudiantes.TabIndex = 7;
+            // 
+            // btnmejoras
+            // 
+            this.btnmejoras.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnmejoras.Location = new System.Drawing.Point(12, 320);
+            this.btnmejoras.Name = "btnmejoras";
+            this.btnmejoras.Size = new System.Drawing.Size(140, 50);
+            this.btnmejoras.TabIndex = 8;
+            this.btnmejoras.Text = "Mostrar el mejor por asignatura ";
+            this.btnmejoras.UseVisualStyleBackColor = true;
+            this.btnmejoras.Click += new System.EventHandler(this.btnmejoras_Click);
+            // 
+            // btnasigna
+            // 
+            this.btnasigna.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnasigna.Location = new System.Drawing.Point(488, 320);
+            this.btnasigna.Name = "btnasigna";
+            this.btnasigna.Size = new System.Drawing.Size(133, 50);
+            this.btnasigna.TabIndex = 9;
+            this.btnasigna.Text = "Promedio de asignatura";
+            this.btnasigna.UseVisualStyleBackColor = true;
+            this.btnasigna.Click += new System.EventHandler(this.btnasigna_Click);
+            // 
+            // btnMJRE
+            // 
+            this.btnMJRE.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMJRE.Location = new System.Drawing.Point(328, 320);
+            this.btnMJRE.Name = "btnMJRE";
+            this.btnMJRE.Size = new System.Drawing.Size(154, 50);
+            this.btnMJRE.TabIndex = 10;
+            this.btnMJRE.Text = "Mostrar mejor estudiante";
+            this.btnMJRE.UseVisualStyleBackColor = true;
+            this.btnMJRE.Click += new System.EventHandler(this.btnMJRE_Click);
+            // 
+            // btnreproa
+            // 
+            this.btnreproa.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnreproa.Location = new System.Drawing.Point(158, 320);
+            this.btnreproa.Name = "btnreproa";
+            this.btnreproa.Size = new System.Drawing.Size(152, 50);
+            this.btnreproa.TabIndex = 11;
+            this.btnreproa.Text = "Mostrar reprobados y aprobados";
+            this.btnreproa.UseVisualStyleBackColor = true;
+            this.btnreproa.Click += new System.EventHandler(this.btnreproa_Click);
+            // 
+            // Quit
+            // 
+            this.Quit.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Quit.Location = new System.Drawing.Point(627, 320);
+            this.Quit.Name = "Quit";
+            this.Quit.Size = new System.Drawing.Size(133, 50);
+            this.Quit.TabIndex = 12;
+            this.Quit.Text = "Salir";
+            this.Quit.UseVisualStyleBackColor = true;
+            this.Quit.Click += new System.EventHandler(this.Quit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Quit);
+            this.Controls.Add(this.btnreproa);
+            this.Controls.Add(this.btnMJRE);
+            this.Controls.Add(this.btnasigna);
+            this.Controls.Add(this.btnmejoras);
+            this.Controls.Add(this.Estudiantes);
+            this.Controls.Add(this.btnmostrar);
+            this.Controls.Add(this.btningresar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtapellido);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtapellido;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtmateD;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtmate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSocio;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtpoo;
+        private System.Windows.Forms.Button btningresar;
+        private System.Windows.Forms.Button btnmostrar;
+        private System.Windows.Forms.ListBox Estudiantes;
+        private System.Windows.Forms.Button btnmejoras;
+        private System.Windows.Forms.Button btnasigna;
+        private System.Windows.Forms.Button btnMJRE;
+        private System.Windows.Forms.Button btnreproa;
+        private System.Windows.Forms.Button Quit;
+    }
+}
+
---
+++ estudiantes66/estudiantes66/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace estudiantes66
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Estudiantes.Items.Add("Nombre"+"\t"+"Apellido"+"\t"+ "P.O.O"+"\t"+"Sociologia  "+"\t"+"Matematica"+"\t"+"Matematicas Discretas"+"\t");
+        }
+
+        void limpiar()
+        {
+            txtapellido.Clear();
+            txtmate.Clear();
+            txtmateD.Clear();
+            txtnombre.Clear();
+            txtpoo.Clear();
+            txtSocio.Clear();
+        }
+        /*Contadores*/
+        int contador;
+        int i;
+        int c;
+        /*Aprovados */
+        int P;
+        int S;
+        int MD;
+        int M;
+        //Reprovados
+        int RP;
+        int RS;
+        int RMD;
+        int RM;
+        //Index stuff
+        int indexP;
+        int indexM;
+        int indexS;
+        int indexMD;
+        //Pos del mas alto
+        int IneMA;
+        //acumuladores  de asignatura
+        float PASIG1=0;/*POO*/
+        float PASIG2 = 0;/*Sociologia*/
+        float PASIG3 = 0;/*Matematica*/
+        float PASIG4 = 0;/*Matematica Discreta*/
+        /*Promedio por asignatura*/
+        float P1;
+        float P2;
+        float P3;
+        float P4;
+        public struct Datos
+        {
+            public int poo;
+            public int Sociologia;
+            public int Matematica;
+            public int MatematicaD;
+            public string Nombre;
+            public string Apellido;
+            public float promedio;
+        }
+
+        Datos[] info;
+
+
+
+        void Mejor()
+        {
+            int MejorP = info[0].poo;
+            for (c = 0; c < info.Length; c++)
+            {
+                if (info[c].poo > MejorP)
+                {
+                    indexP = c;
+                }
+                int MejorM = info[0].Matematica;
+                if (info[c].Matematica > MejorM)
+                {
+                    indexM = c;
+                }
+                int MejorS = info[0].Sociologia;
+                if (info[c].Sociologia > MejorS)
+                {
+                    indexS = c;
+                }
+                int MejorMD = info[0].MatematicaD;
+                if (info[c].MatematicaD > MejorMD)
+                {
+                    indexMD = c;
+                }
+            }
+            MessageBox.Show("El mejor estudiante es en POO es: " + info[indexP].Nombre);
+            MessageBox.Show("El mejor estudiante es en Sociologia es: " + info[indexS].Nombre);
+            MessageBox.Show("El mejor estudiante es en Matematica es: " + info[indexM].Nombre);
+            MessageBox.Show("El mejor estudiante es en Matematica Discreta es: " + info[indexMD].Nombre);
+        }/*Final de esto*/
+
+        void Repro_y_apro()
+        {
+            for (c = 0; c < info.Length; c++)
+            {
+                if (info[c].poo > 60)
+                {
+                    P++;
+                }
+
+                if (info[c].Sociologia > 60)
+                {
+                    S++;
+                }
+
+                if (info[c].Matematica > 60)
+                {
+                    M++;
+                }
+
+                if (info[c].MatematicaD > 60)
+                {
+                    MD++;
+                }
+
+                //Reprovados
+                if (info[c].poo < 60)
+                {
+                    RP++;
+                }
+
+                if (info[c].Sociologia < 60)
+                {
+                    RS++;
+                }
+
+                if (info[c].Matematica < 60)
+                {
+                    RM++;
+                }
+
+                if (info[c].MatematicaD < 60)
+                {
+                    RMD++;
+                }
+            }
+            MessageBox.Show("El numero de aprovados en P.O.O es: "+Convert.ToString(P));
+            MessageBox.Show("El numero de reprovados en P.O.O es: " + Convert.ToString(RP));
+            MessageBox.Show("El numero de aprovados en Sociologia es: " + Convert.ToString(S));
+            MessageBox.Show("El numero de reprovados en Sociologia es: " + Convert.ToString(RS));
+            MessageBox.Show("El numero de aprovados en Matematica es: " + Convert.ToString(M));
+            MessageBox.Show("El numero de reprovados en Matematica es: " + Convert.ToString(RM));
+            MessageBox.Show("El numero de aprovados en Matematicas Discretas es: " + Convert.ToString(MD));
+            MessageBox.Show("El numero de reprovados en Matematica Discretas es: " + Convert.ToString(RMD));
+        }/*Fin de esto*/
+
+        void promedios()
+        {
+            for (c = 0; c < info.Length; c++)
+            {
+                info[c].promedio = info[c].poo + info[c].Sociologia + info[c].Matematica + info[i].MatematicaD / 4;
+            }
+        }/*fin de esto*/
+
+        void promasalto()
+        {
+            //Promedio mas alto
+            float Promas = info[0].promedio;
+            for (c = 0; c < info.Length; c++)
+            {
+                if (info[0].promedio > Promas)
+                {
+                    IneMA = c;
+                }
+            }
+            MessageBox.Show("El promedio mas alto lo tiene: " + info[IneMA].Nombre+"y es de: "+Promas);
+        }/*Fin de esto*/
+
+        void prom_asig()
+        {
+            for (c = 0; c < info.Length; c++)
+            {
+                contador++;/*Numero de estudiantes*/
+            }
+
+            for (c = 0; c < info.Length; c++)
+            {
+                PASIG1 = info[c].poo + PASIG1;
+                PASIG2 = info[c].Sociologia + PASIG2;
+                PASIG3 = info[c].Matematica + PASIG3;
+                PASIG4 = info[c].MatematicaD + PASIG4;
+            }
+            /*Promedio*/
+            P1 = PASIG1 / contador;
+            P2 = PASIG2 / contador;
+            P3 = PASIG3 / contador;
+            P4 = PASIG4 / contador;
+            MessageBox.Show("El promedio de la asignatura de P.O.O es: " + Convert.ToString(P1)+"\t"+
+                "El promedio de la asignatura de Matematica es: "+Convert.ToString(P3)+"\t"+
+                "El promedio de la asignatura de Sociologia es: "+Convert.ToString(P2)+"\t"+
+                "El promedio de la asignatura de Matematicas Discretas es: "+Convert.ToString(P4));
+        }/*Fin de esto*/
+        private void btningresar_Click(object sender, EventArgs e)
+        {
+            /*Asignacion de cosas*/
+            Array.Resize(ref info, i + 1);
+            info[i].Nombre = txtnombre.Text;
+            info[i].Apellido = txtapellido.Text;
+            info[i].poo = int.Parse(txtpoo.Text);
+            info[i].Sociologia = int.Parse(txtSocio.Text);
+            info[i].Matematica = int.Parse(txtmate.Text);
+            info[i].MatematicaD = int.Parse(txtmateD.Text);
+            promedios();
+            limpiar();
+            i++;
+        }
+
+        private void btnmejoras_Click(object sender, EventArgs e)
+        {
+            Mejor();
+        }
+
+        private void btnreproa_Click(object sender, EventArgs e)
+        {
+            Repro_y_apro();
+        }
+
+        private void Quit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMJRE_Click(object sender, EventArgs e)
+        {
+            promasalto();
+        }
+
+        private void btnasigna_Click(object sender, EventArgs e)
+        {
+            prom_asig();
+        }
+
+        private void btnmostrar_Click(object sender, EventArgs e)
+        {
+            Estudiantes.Items.Clear();
+            for (c = 0; c < info.Length; c++)
+            {
+                Estudiantes.Items.Add(info[c].Nombre+"\t"+info[c].Apellido+"\t"+info[c].poo+"\t"+info[c].Sociologia+"\t"+info[c].Matematica+"\t"+info[c].MatematicaD
+                    +"\t"+info[c].promedio);
+            }
+        }
+    }
+}
